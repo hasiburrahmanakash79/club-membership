38c49e967a1d53b6c76a5f7934623b7b3f418223
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-04-04 11:18:39 +0600

    add navbar

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavLink = ({route}) => {
     const {name, path} = route
     return (
-        <li className='list-none md:mr-12 hover:bg-teal-700'>
+        <li className='list-none md:mr-12 hover:underline'>
             <a href={path}>{name}</a>
         </li>
     );
--- src/components/PriceCart/PriceCart.jsx
+++ src/components/PriceCart/PriceCart.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 const PriceCart = ({ price }) => {
-  const { name, amount, description } = price;
+  const { name, amount, features } = price;
   return (
-    <div className="bg-teal-500 rounded-lg p-6">
+    <div className="bg-teal-500 hover:bg-teal-600 rounded-lg p-6">
       <div>
         <h1>
           <span className="text-6xl font-extrabold">{amount}</span>
           <span>/month</span>{" "}
         </h1>
         <h1 className="font-bold text-4xl my-4">{name}</h1>
-        <h1 className="text-2xl font-semibold">{description}</h1>
+        <p>
+          {
+            features.map(feature => <li>{feature}</li>)
+          }
+        </p>
       </div>
       <div className="mt-5">
         <button className="bg-teal-800 p-2 text-cyan-50 text-lg font-bold rounded-md hover:bg-teal-900">BUY NOW</button>
